81ea83b178fdb3815657f791eb7fad8bb8c7a2b0
Wei Xin Koh <e0201350@u.nus.edu>
2020-04-01 16:42:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// // Add your doToElementsInArray() function here:
-// function doToElementsInArray() {
-//
-// }
-//
-// // Add your changeCompletely() function here:
-// function changeCompletely() {
-//
-// }
+// Add your doToElementsInArray() function here:
+function doToElementsInArray() {
+  
+}
+
+// Add your changeCompletely() function here:
+function changeCompletely() {
+  
+}
